f2fa9ac43ce83d48eafd7c38e9c38018c92f1a1c
ZiganshinRS <ZiganshinRS@gmail.com>
2026-04-09 10:46:59 +0300

    Add files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+function info() {
+    const m = new Map();
+    m.set('name', 'ajs');
+    m.set('version', '1.0.0');
+    return m;
+}
+
+/*
+export default {
+    info
+};
+ */
+
+module.exports = {
+    info
+};
